be4ddf2af6c4c2bd43edb783fbc4222f8c2cc853
AdleeAfif <kamitokazuya@gmail.com>
2022-09-26 16:59:13 +0800

    Added new css for specific file/page

--- src/layout/MainNavigation.js
+++ src/layout/MainNavigation.js
@@ -1,8 +1,10 @@
 import { Link } from 'react-router-dom';
+import classes from './MainNavigation.module.css';
+
 
 function MainNavigation() {
-    return <header>
-        <div>
+    return <header className={classes.header}>
+        <div className={classes.logo}>
             React Meetups
         </div>
         <nav>

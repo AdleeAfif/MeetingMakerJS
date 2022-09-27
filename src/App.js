125a357856d7618515849b05c4e4d528d73e3ad5
AdleeAfif <kamitokazuya@gmail.com>
2022-09-27 15:36:26 +0800

    Improve Card visualization. Added Layout component and uses props.children to pass the data.

--- src/App.js
+++ src/App.js
@@ -3,19 +3,17 @@ import { Routes } from 'react-router-dom';
 import AllMeetups from './pages/AllMeetups';
 import NewMeetup from './pages/NewMeetup';
 import Favourites from './pages/Favourites';
-import MainNavigation from './layout/MainNavigation';
+import Layout from './layout/Layout';
 
 function App() {
   return (
-    <div>
-      <MainNavigation />
-      <Routes>
-        <Route path="/" element={<AllMeetups />} />
-        <Route path="/new-meetup" element={<NewMeetup />} />
-        <Route path="/favourites" element={<Favourites />} />
-      </Routes>
-
-    </div>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<AllMeetups />} />
+          <Route path="/new-meetup" element={<NewMeetup />} />
+          <Route path="/favourites" element={<Favourites />} />
+        </Routes>
+      </Layout>
   );
 }
 

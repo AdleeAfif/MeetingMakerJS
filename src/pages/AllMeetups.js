9a0af87bab871046187dcf0b40dc594eae0f67f9
AdleeAfif <kamitokazuya@gmail.com>
2022-10-04 16:10:29 +0800

    Load meetups data from database to the main page. Used useState and useEffect.

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,34 +1,50 @@
+import { useState, useEffect } from 'react';
 import MeetupList from "../components/MeetupList";
-
-const DUMMY_DATA = [
-    {
-      id: 'm1',
-      title: 'This is a first meetup',
-      image:
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-      address: 'Meetupstreet 5, 12345 Meetup City',
-      description:
-        'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-    },
-    {
-      id: 'm2',
-      title: 'This is a second meetup',
-      image:
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-      address: 'Meetupstreet 5, 12345 Meetup City',
-      description:
-        'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-    },
-  ];
 
 function AllMeetups() {
 
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(
+      'https://react-second-app-f5cab-default-rtdb.firebaseio.com/meetups.json'
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const meetups = [];
+
+        for (const key in data) {
+          const meetup = {
+            id: key,
+            ...data[key]
+          };
+
+          meetups.push(meetup);
+        }
+
+        setIsLoading(false);
+        setLoadedMeetups(meetups);
+      });
+  }, []);
+
+  if (isLoading) {
     return (
-        <section>
-            <h1>All Meetups</h1>;
-            <MeetupList meetups={DUMMY_DATA} />
-        </section>
+      <section>
+        <p>LOADING SET DATA...</p>
+      </section>
     );
+  }
+
+  return (
+    <section>
+      <h1>All Meetups</h1>
+      <MeetupList meetups={loadedMeetups} />
+    </section>
+  );
 }
 
 export default AllMeetups;

f5a1bd40fd986a78c34c2fb30abfdfdb1962d9e6
AdleeAfif <kamitokazuya@gmail.com>
2022-10-04 15:32:47 +0800

    Sending POST request to the database server.

--- src/components/NewMeetupForm.js
+++ src/components/NewMeetupForm.js
@@ -2,7 +2,7 @@ import Card from '../interface/Card';
 import classes from './NewMeetupForm.module.css';
 import { useRef } from 'react';
 
-function NewMeetupForm() {
+function NewMeetupForm(props) {
     const titleInputRef = useRef();
     const imageInputRef = useRef();
     const addressInputRef = useRef();
@@ -23,7 +23,7 @@ function NewMeetupForm() {
             description: enteredDescription,
         };
 
-        console.log(meetupData);
+        props.onAddMeetup(meetupData);
     }
 
     return <Card>
--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,25 @@
+import { useNavigate } from 'react-router-dom';
 import NewMeetupForm from "../components/NewMeetupForm";
 
 function NewMeetup() {
+    const navigate = useNavigate();
+
+    function addMeetupHandler(meetupData){
+        fetch('https://react-second-app-f5cab-default-rtdb.firebaseio.com/meetups.json',
+        {
+            method: 'POST',
+            body: JSON.stringify(meetupData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(() => {
+            navigate("/", { replace: true });
+        });
+    }
 
     return <section>
         <h1>Add New Meetup</h1>
-        <NewMeetupForm />
+        <NewMeetupForm onAddMeetup={addMeetupHandler}/>
 
     </section>;
 }

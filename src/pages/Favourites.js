a0829051af1ea2b241d1b11cdbf908a78d6aaaf2
AdleeAfif <kamitokazuya@gmail.com>
2022-10-08 13:45:14 +0800

    setting up the favourite page using context

--- src/pages/Favourites.js
+++ src/pages/Favourites.js
@@ -1,6 +1,26 @@
-function Favourites() {
+import { useContext } from 'react';
+import FavouritesContext from '../store/favourite-context';
+import MeetupList from '../components/MeetupList';
 
-    return <div>Favourites Page</div>;
+function Favourites() {
+    const favContext = useContext(FavouritesContext);
+
+    let content;
+
+    if (favContext.totalFavourites === 0){
+        content = <p>No Favourites Is Added Yet. Start Adding!</p>
+    }
+    else{
+        content = <MeetupList meetups={favContext.favourites}/>
+    }
+
+    return <section>
+        <h1>FAVOURITES</h1>
+        {content}
+    </section>;
 }
 
 export default Favourites;
+
+//It is important to know that all this favs will be lost when reloaded as it only use useState and useContext memory.
+//It can be saved by using browser storage or server to be permanent.

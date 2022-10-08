36a9045421143ee05a3aa6f549820ad1370f26f7
AdleeAfif <kamitokazuya@gmail.com>
2022-10-08 13:00:49 +0800

    wrapping the whole app with fav context function.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
+import { FavouritesContextProvider } from './store/favourite-context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <FavouritesContextProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </FavouritesContextProvider>
 );
